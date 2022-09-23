74118d84798cb3d416bc3e88de552c89989a1255
asunder9 <ahmedntrance@gmail.com>
2022-09-23 08:00:35 +0800

    update to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
+import welcomeImg from "../../assets/media/welcome-banner.png";
 
 const Dashboard = (props) => {
     const history = useHistory();
@@ -8,23 +9,18 @@ const Dashboard = (props) => {
 
     return (
         <div className="col-12 flex flex-column align-items-center">
-            <div className="col-10">
-                <h5 className="mb-0 ml-2">Dashboard</h5>
+            <div className="w-full flex flex-column align-items-center">
+                <img src={welcomeImg} alt="welcome image" className="h-30rem" />
+                <p className="text-7xl">Welcome!</p>
             </div>
-            <div className="grid col-10">
-                <div className="col-12 lg:col-6 xl:col-4">
-                    <Link to="">
-                        <div className="card mb-0 flex flex-column align-items-center justify-content-center hover zoom" style={{ height: "10rem" }}>
-                            <i className="pi pi-plus mb-2" style={{ fontSize: "1.2rem" }}></i>
-                            <div className="text-900 font-medium text-lg">Welcome!</div>
-                        </div>
-                    </Link>
-                </div>
-            </div>
+            <div></div>
+            <div className="grid col-10">{/* ~cb-add-services-links~ */}</div>
         </div>
     );
 };
-const mapState = (state) => ({});
+const mapState = (state) => ({
+    //
+});
 const mapDispatch = (dispatch) => ({
     //
 });
